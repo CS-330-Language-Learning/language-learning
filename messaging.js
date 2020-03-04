48560b7e231024ec1b488bc9ab5249354c1aef40
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2020-03-04 14:00:48 -0600

    Add mock playback functionality

--- messaging.js
+++ messaging.js
@@ -26,6 +26,7 @@ function handleYesClick(evt) {
     msg.style.visibility = 'visible'
     const msgLabel = document.getElementById('m4-label')
     msgLabel.style.visibility = 'visible'
+    messageRecorded = false
 }
 
 function handleNoClick(evt) {
@@ -38,6 +39,7 @@ function handleNoClick(evt) {
     yesButton.style.display = 'none'
     const noButton = document.getElementById('no')
     noButton.style.display = 'none'
+    messageRecorded = false
 
 }
 
@@ -48,7 +50,6 @@ function handleStart(evt) {
         micButton.classList.add('pulse')
         const hintText = document.getElementById('hint-text')
         hintText.innerHTML = 'Playing...'
-        // TODO disable the buttons and show the audio playing?
     } else {
         const micButton = document.getElementById('audio')
         micButton.classList.add('pulse')
@@ -59,14 +60,19 @@ function handleStart(evt) {
 
 function handleEnd(evt) {
     evt.preventDefault()
+    const micButton = document.getElementById('audio')
+    const hintText = document.getElementById('hint-text')
     if (messageRecorded) {
-        console.log('play back a message')
+        micButton.classList.add('disabled')
+        setTimeout(function() {
+            micButton.classList.remove('pulse')
+            micButton.classList.remove('disabled')
+            hintText.innerHTML = 'Preview'
+        }, 1000)
     } else {
-        const micButton = document.getElementById('audio')
         micButton.classList.remove('pulse')
         const icon = document.getElementById('audio-icon')
         icon.innerHTML = 'play_arrow'
-        const hintText = document.getElementById('hint-text')
         hintText.innerHTML = 'Preview'
         const yesButton = document.getElementById('yes')
         yesButton.style.display = 'inline'
@@ -75,18 +81,3 @@ function handleEnd(evt) {
         messageRecorded = true
     }
 }
-
-// function openfile(file) { window.location = file }
-
-// const toProfile = () => {
-//     openfile('./profile2.html')
-// }
-
-// const sendMessage = () => {
-//     document.querySelector('#testSend').style.visibility = 'visible'
-//     document.querySelector('.recordButton').innerText = 'Record Message'
-// }
-
-// const recording = () => {
-//     document.querySelector('.recordButton').innerText = 'recording'
-// }
